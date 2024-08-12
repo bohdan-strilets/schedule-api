511868767ef956d5d927982ba59cce681508ff49
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-08-12 20:54:01 +0200

    Added CORS for client.

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
+	app.enableCors({ origin: process.env.CLIENT_URL, credentials: true })
 	app.useGlobalPipes(new ValidationPipe())
 	app.setGlobalPrefix('api')
 	await app.listen(process.env.PORT || 4000)
