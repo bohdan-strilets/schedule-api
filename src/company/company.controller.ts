db381680728228458d0f06fa0847e7765fabccd7
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-08-18 16:10:21 +0200

    Typed methods for working with the companies.

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -6,12 +6,16 @@ import {
 	Param,
 	Patch,
 	Post,
+	Res,
 } from '@nestjs/common'
+import { Response } from 'express'
 import { Auth } from 'src/auth/decorators/auth.decorator'
+import { ResponseType } from 'src/common/response.type'
 import { User } from 'src/user/decorators/user.decorator'
 import { CompanyService } from './company.service'
 import { CreateCompanyDto } from './dto/create-company.dto'
 import { UpdateCompanyDto } from './dto/update-company.dto'
+import { CompanyModel } from './models/company.model'
 
 @Auth()
 @Controller('company')
@@ -19,35 +23,88 @@ export class CompanyController {
 	constructor(private readonly companyService: CompanyService) {}
 
 	@Post('create')
-	async create(@Body() dto: CreateCompanyDto, @User('_id') _id: string) {
-		return await this.companyService.create(_id, dto)
+	async create(
+		@Body() dto: CreateCompanyDto,
+		@User('_id') _id: string,
+		@Res() res: Response
+	): Promise<ResponseType<CompanyModel>> {
+		const data = await this.companyService.create(_id, dto)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 
 	@Patch('update/:companyId')
 	async update(
 		@Body() dto: UpdateCompanyDto,
-		@Param('companyId') companyId: string
-	) {
-		return await this.companyService.update(companyId, dto)
+		@Param('companyId') companyId: string,
+		@Res() res: Response
+	): Promise<ResponseType<CompanyModel>> {
+		const data = await this.companyService.update(companyId, dto)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 
 	@Delete('delete/:companyId')
-	async delete(@Param('companyId') companyId: string) {
-		return await this.companyService.delete(companyId)
+	async delete(
+		@Param('companyId') companyId: string,
+		@Res() res: Response
+	): Promise<ResponseType> {
+		const data = await this.companyService.delete(companyId)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 
 	@Delete('delete-all')
-	async deleteAll(@User('_id') _id: string) {
-		return await this.companyService.deleteAll(_id)
+	async deleteAll(
+		@User('_id') _id: string,
+		@Res() res: Response
+	): Promise<ResponseType> {
+		const data = await this.companyService.deleteAll(_id)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 
 	@Get('by-id/:companyId')
-	async getById(@Param('companyId') companyId: string) {
-		return await this.companyService.getById(companyId)
+	async getById(
+		@Param('companyId') companyId: string,
+		@Res() res: Response
+	): Promise<ResponseType<CompanyModel>> {
+		const data = await this.companyService.getById(companyId)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 
 	@Get('all')
-	async getAll(@User('_id') _id: string) {
-		return await this.companyService.getAll(_id)
+	async getAll(
+		@User('_id') _id: string,
+		@Res() res: Response
+	): Promise<ResponseType<CompanyModel[]>> {
+		const data = await this.companyService.getAll(_id)
+
+		if (!data.success) {
+			res.status(data.statusCode)
+		}
+
+		return data
 	}
 }
--- src/company/company.service.ts
+++ src/company/company.service.ts
@@ -1,10 +1,7 @@
-import {
-	BadRequestException,
-	Injectable,
-	NotFoundException,
-} from '@nestjs/common'
+import { HttpStatus, Injectable } from '@nestjs/common'
 import { ModelType } from '@typegoose/typegoose/lib/types'
 import { InjectModel } from 'nestjs-typegoose'
+import { ResponseType } from 'src/common/response.type'
 import { CompanyLogoUrl } from 'src/common/vars/company-logo'
 import { ErrorMessages } from 'src/common/vars/error-messages'
 import { CreateCompanyDto } from './dto/create-company.dto'
@@ -18,51 +15,109 @@ export class CompanyService {
 		private readonly CompanyModel: ModelType<CompanyModel>
 	) {}
 
-	async create(userId: string, dto: CreateCompanyDto) {
+	async create(
+		userId: string,
+		dto: CreateCompanyDto
+	): Promise<ResponseType<CompanyModel>> {
 		this.checkDto(dto)
 		const data = { ...dto, owner: userId, logoUrl: CompanyLogoUrl }
-		return await this.CompanyModel.create(data)
+		const company = await this.CompanyModel.create(data)
+
+		return {
+			success: true,
+			statusCode: HttpStatus.CREATED,
+			data: company,
+		}
 	}
 
-	async update(companyId: string, dto: UpdateCompanyDto) {
+	async update(
+		companyId: string,
+		dto: UpdateCompanyDto
+	): Promise<ResponseType<CompanyModel>> {
 		this.checkDto(dto)
 		await this.checkCompanyFromDb(companyId)
 
-		return await this.CompanyModel.findByIdAndUpdate(companyId, dto, {
-			new: true,
-		})
+		const updatedCompany = await this.CompanyModel.findByIdAndUpdate(
+			companyId,
+			dto,
+			{
+				new: true,
+			}
+		)
+
+		return {
+			success: true,
+			statusCode: HttpStatus.OK,
+			data: updatedCompany,
+		}
 	}
 
-	async delete(companyId: string) {
+	async delete(companyId: string): Promise<ResponseType> {
 		await this.checkCompanyFromDb(companyId)
 		await this.CompanyModel.findByIdAndDelete(companyId)
-		return
+
+		return {
+			success: true,
+			statusCode: HttpStatus.OK,
+		}
 	}
 
-	async deleteAll(userId: string) {
+	async deleteAll(userId: string): Promise<ResponseType> {
 		await this.CompanyModel.deleteMany({ owner: userId })
-		return
+
+		return {
+			success: true,
+			statusCode: HttpStatus.OK,
+		}
 	}
 
-	async getById(companyId: string) {
-		return await this.checkCompanyFromDb(companyId)
+	async getById(companyId: string): Promise<ResponseType<CompanyModel>> {
+		const company = await this.checkCompanyFromDb(companyId)
+
+		if ('_id' in company) {
+			return {
+				success: true,
+				statusCode: HttpStatus.OK,
+				data: company,
+			}
+		}
 	}
 
-	async getAll(userId: string) {
-		return await this.CompanyModel.find({ owner: userId })
+	async getAll(userId: string): Promise<ResponseType<CompanyModel[]>> {
+		const companies = await this.CompanyModel.find({ owner: userId })
+
+		return {
+			success: true,
+			statusCode: HttpStatus.OK,
+			data: companies,
+		}
 	}
 
 	// HELPERS
 
-	private async checkCompanyFromDb(companyId: string) {
+	private async checkCompanyFromDb(
+		companyId: string
+	): Promise<CompanyModel | ResponseType> {
 		const companyFromDb = await this.CompanyModel.findById(companyId)
-		if (!companyFromDb)
-			throw new NotFoundException(ErrorMessages.NOT_FOUND_BY_ID)
+
+		if (!companyFromDb) {
+			return {
+				success: false,
+				statusCode: HttpStatus.NOT_FOUND,
+				message: ErrorMessages.NOT_FOUND_BY_ID,
+			}
+		}
 
 		return companyFromDb
 	}
 
 	private checkDto(dto: any) {
-		if (!dto) throw new BadRequestException(ErrorMessages.BAD_REQUEST)
+		if (!dto) {
+			return {
+				success: false,
+				statusCode: HttpStatus.BAD_REQUEST,
+				message: ErrorMessages.BAD_REQUEST,
+			}
+		}
 	}
 }
