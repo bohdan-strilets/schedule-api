9e3a34394d3bfd293330356117635d7e80bb3b8c
BS <bohdan.strilets@gmail.com>
2024-09-25 14:07:11 +0200

    delete global error handler

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,11 @@ import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
 import * as cookieParser from 'cookie-parser'
 import { AppModule } from './app.module'
-import { AllExceptionsFilter } from './errors/exceptions-filter'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule)
 	app.use(cookieParser())
 	app.enableCors({ origin: process.env.CLIENT_URL, credentials: true })
-	app.useGlobalFilters(new AllExceptionsFilter())
 	app.useGlobalPipes(new ValidationPipe())
 	app.setGlobalPrefix('api')
 	await app.listen(process.env.PORT || 4000)
